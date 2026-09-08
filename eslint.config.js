d22098afaaa6e88ead0324cba8443947c6f57761
alaangc <54085508+alaangc@users.noreply.github.com>
2026-09-07 18:39:03 -0700

    fix(lint): exclude local tool installations

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   {
     ignores: [
+      '.tools/**',
       '**/node_modules/**',
       '**/dist/**',
       '**/build/**',
